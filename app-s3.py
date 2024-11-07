24b4b0c4d524665e1e1f065f2626344418137646
arvindaggmca <arvind.bcamca@gmail.com>
2024-11-06 20:21:15 -0500

    Update app-s3.py

--- app-s3.py
+++ app-s3.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         .appName("PythonWordCount")\
         .getOrCreate()
 
-    textfile = spark.read.text("s3a://192.168.121.211:8080/penasio/spark-test")
+    textfile = spark.read.text("s3://myemrtestingbucket11052024/testing/chat.txt")
 
     lines = textfile.rdd.map(lambda r: r[0])
     counts = lines.flatMap(lambda x: x.split(' ')) \
